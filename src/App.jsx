76ef1c7838548c169afc0183c5de5caccd3c5d7e
esang222 <carinagajessa@gmail.com>
2025-06-03 20:05:52 +0800

    Login Form Frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import Login from './Login';
 
-function App() {
+const App = () => {
   return (
     <div>
-      <h1>Welcome</h1>
+      <Login />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Login.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import logo from "./assets/envirocool-logo.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Login = () => {
+  return (
+    <div className="login-container">
+      <div className="login-card text-center">
+        <img
+          src={logo}
+          alt="EnviroCool Logo"
+          className="login-logo img-fluid mb-4"
+          width="300"
+        />
+
+        <form className="login-form text-start">
+          <div className="mb-3">
+            <label htmlFor="username" className="form-label">
+              Username:
+            </label>
+            <input type="text" id="username" className="form-control" />
+          </div>
+
+          <div className="mb-1">
+            <label htmlFor="password" className="form-label">
+              Password:
+            </label>
+            <input type="password" id="password" className="form-control" />
+          </div>
+
+          <div className="mb-5">
+            <a href="#" className="small fw-semibold">
+              Forgot password?
+            </a>
+          </div>
+
+          <button type="submit" className="btn login-btn w-100 rounded-3 fs-5 p-2">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
