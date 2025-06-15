be42e31571c02bd822214bf8fd7d59cae7eee976
Liezel Paciente <pacienteliezel04@gmail.com>
2025-06-15 21:45:33 +0800

    fixed create personnelAccount b

--- src/CreatePersonnelAccount.jsx
+++ src/CreatePersonnelAccount.jsx
@@ -1,22 +1,11 @@
-import logo from "./assets/envirocool-logo.png";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import React, { useState, useEffect } from "react";
-import {
-  FaClipboardList,
-  FaCog,
-  FaSignOutAlt,
-  FaSearch,
-  FaUserPlus,
-  FaBars,
-  FaTimes,
-} from "react-icons/fa";
-import { Row, Col, Container, Button, Form } from "react-bootstrap";
+import OperationalLayout from "./OperationalLayout";
 
 const CreatePersonnelAccount = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   const navigate = useNavigate();
-  const [errorMessage, setErrorMessage] = useState("");
+
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
@@ -27,7 +16,10 @@ const CreatePersonnelAccount = () => {
     email: "",
   });
 
+  const [errorMessage, setErrorMessage] = useState("");
+
   useEffect(() => {
+    document.title = "Create Delivery Personnel Account";
     if (errorMessage) {
       const timer = setTimeout(() => setErrorMessage(""), 3000);
       return () => clearTimeout(timer);
@@ -42,8 +34,6 @@ const CreatePersonnelAccount = () => {
       const today = new Date();
       let calculatedAge = today.getFullYear() - birthDate.getFullYear();
       const monthDiff = today.getMonth() - birthDate.getMonth();
-
-      // Adjust age if birthday hasn't occurred yet this year
       if (
         monthDiff < 0 ||
         (monthDiff === 0 && today.getDate() < birthDate.getDate())
@@ -92,12 +82,13 @@ const CreatePersonnelAccount = () => {
         formData,
         { headers: { "Content-Type": "application/json" } }
       );
+
       const data = res.data;
       console.log("Response from PHP:", data);
 
       switch (data.status) {
         case "success":
-          alert(`Delivery Personnel Account created successfully!`);
+          alert("Delivery Personnel Account created successfully!");
           navigate("/operational-delivery-details");
           break;
         case "email_exists":
@@ -115,9 +106,6 @@ const CreatePersonnelAccount = () => {
         case "db_error":
           setErrorMessage("Database error. Please try again.");
           break;
-        case "general_error":
-          setErrorMessage("Unexpected server error.");
-          break;
         default:
           setErrorMessage(data.message || "Unknown error occurred.");
           break;
@@ -128,208 +116,128 @@ const CreatePersonnelAccount = () => {
     }
   };
 
-  const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
-
-  const handleCreateAccount = () => navigate("/create-personnel-account");
-  const handleDeliveryDetails = () => navigate("/operational-delivery-details");
-  const handleSettings = () => navigate("/settings");
-  const handleLogout = () => navigate("/");
-
   return (
-    <div className="d-flex" style={{ minHeight: "100vh" }}>
-      {/* SIDEBAR */}
-      <aside
-        className={`sidebar d-flex flex-column align-items-center p-3 ${
-          isSidebarOpen ? "show" : "collapsed"
-        }`}
-      >
-        {/* CLOSE BUTTON */}
-        <button
-          className="btn close-sidebar d-lg-none align-self-end mb-3"
-          onClick={toggleSidebar}
-        >
-          <FaTimes />
-        </button>{" "}
-        <img
-          src={logo}
-          alt="Envirocool Logo"
-          className="logo mb-4 img-fluid"
-          width="250px"
-        />
-        {/* NAVIGATIONS  */}
-        <nav className="nav-buttons">
-          <button className="nav-btn" onClick={handleCreateAccount}>
-            <FaUserPlus className="icon" /> CREATE DELIVERY PERSONNEL ACCOUNT
-          </button>
-          <button className="nav-btn" onClick={handleDeliveryDetails}>
-            <FaClipboardList className="icon" /> DELIVERY DETAILS
-          </button>
-          <button className="nav-btn" onClick={handleSettings}>
-            <FaCog className="icon" /> SETTINGS
-          </button>
-          <button className="nav-btn logout" onClick={handleLogout}>
-            <FaSignOutAlt className="icon" /> LOGOUT
-          </button>
-        </nav>
-      </aside>
-
-      {/* MAIN CONTENT  */}
-      <main className="main-panel flex-grow-1 p-4">
-        {/* HEADER  */}
-        <div className="dashboard-header d-flex justify-content-between align-items-center">
-          {/* TOGGLE  BUTTON */}
-          <div className="d-flex align-items-center">
-            <button className="btn d-lg-none me-0" onClick={toggleSidebar}>
-              {isSidebarOpen ? <FaTimes /> : <FaBars />}
-            </button>
-            <h2 className="fs-1 fw-bold m-0">
-              Create Delivery Personnel Account
-            </h2>
-          </div>
-
-          {/* SEARCH BAR */}
-          <div className="search-bar position-relative me-3">
-            <input type="text" placeholder="Search..." />
-            <FaSearch className="search-icon" />
-          </div>
-        </div>
-
-        <div className="d-flex justify-content-between mx-4 my-5">
-          {/* <button
-            className="back btn rounded-2 px-1 py-1 fs-4"
-            onClick={() => navigate(-1)}
-          >
-            <FaArrowLeft className="me-2" />
-          </button> */}
-        </div>
-        <div className="d-flex justify-content-between mx-4 my-5">
-          <Container className="personnel-form-container">
-            <div className="form-card p-4">
-              <h2 className="text-center mb-4">PERSONAL DETAILS</h2>
-              <hr />
-              <Form onSubmit={handleSubmit}>
-                <Row className="mb-3">
-                  <Col md={4}>
-                    <Form.Group>
-                      <Form.Label>
-                        First Name <span className="text-danger">*</span>
-                      </Form.Label>
-                      <Form.Control
-                        type="text"
-                        name="firstName"
-                        value={formData.firstName}
-                        onChange={handleChange}
-                      />
-                    </Form.Group>
-                  </Col>
-                  <Col md={4}>
-                    <Form.Group>
-                      <Form.Label>
-                        Last Name <span className="text-danger">*</span>
-                      </Form.Label>
-                      <Form.Control
-                        type="text"
-                        name="lastName"
-                        value={formData.lastName}
-                        onChange={handleChange}
-                      />
-                    </Form.Group>
-                  </Col>
-                  <Col md={4}>
-                    <Form.Group>
-                      <Form.Label>
-                        Gender <span className="text-danger">*</span>
-                      </Form.Label>
-                      <Form.Control
-                        type="text"
-                        name="gender"
-                        value={formData.gender}
-                        onChange={handleChange}
-                      />
-                    </Form.Group>
-                  </Col>
-                </Row>
-
-                <Row className="mb-3">
-                  <Col md={4}>
-                    <Form.Group>
-                      <Form.Label>
-                        Birthdate <span className="text-danger">*</span>
-                      </Form.Label>
-                      <Form.Control
-                        type="date"
-                        name="birthdate"
-                        value={formData.birthdate}
-                        onChange={handleChange}
-                      />
-                    </Form.Group>
-                  </Col>
-                  <Col md={4}>
-                    <Form.Group>
-                      <Form.Label>
-                        Age <span className="text-danger">*</span>
-                      </Form.Label>
-                      <Form.Control
-                        type="number"
-                        name="age"
-                        value={formData.age}
-                        onChange={handleChange}
-                      />
-                    </Form.Group>
-                  </Col>
-                  <Col md={4}>
-                    <Form.Group>
-                      <Form.Label>
-                        Contact Number <span className="text-danger">*</span>
-                      </Form.Label>
-                      <Form.Control
-                        type="text"
-                        name="contactNumber"
-                        value={formData.contactNumber}
-                        onChange={handleChange}
-                      />
-                    </Form.Group>
-                  </Col>
-                </Row>
-
-                <Row className="mb-4">
-                  <Col md={6}>
-                    <Form.Group>
-                      <Form.Label>
-                        Email <span className="text-danger">*</span>
-                      </Form.Label>
-                      <Form.Control
-                        type="email"
-                        name="email"
-                        value={formData.email}
-                        onChange={handleChange}
-                      />
-                    </Form.Group>
-                  </Col>
-                </Row>
-
-                {errorMessage && (
-                  <div className="text-danger text-center mb-3 fw-bold">
-                    {errorMessage}
-                  </div>
-                )}
-
-                <div className="text-end">
-                  <Button
-                    variant="primary"
-                    type="submit"
-                    className="add-btn bg-success"
-                  >
-                    Proceed
-                  </Button>
-                </div>
-              </Form>
+    <OperationalLayout title="Create Delivery Personnel Account">
+      <div className="add-delivery-form-container mt-5 mx-auto">
+        <div className="form-card mx-auto">
+          <h2 className="form-title">PERSONAL DETAILS</h2>
+          <hr />
+          <form className="personnel-form" onSubmit={handleSubmit}>
+            <div className="form-row">
+              <div className="form-group">
+                <label htmlFor="firstName">
+                  First Name <span className="required">*</span>
+                </label>
+                <input
+                  type="text"
+                  id="firstName"
+                  name="firstName"
+                  value={formData.firstName}
+                  onChange={handleChange}
+                  placeholder="First Name"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="lastName">
+                  Last Name <span className="required">*</span>
+                </label>
+                <input
+                  type="text"
+                  id="lastName"
+                  name="lastName"
+                  value={formData.lastName}
+                  onChange={handleChange}
+                  placeholder="Last Name"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="gender">
+                  Gender <span className="required">*</span>
+                </label>
+                <input
+                  type="text"
+                  id="gender"
+                  name="gender"
+                  value={formData.gender}
+                  onChange={handleChange}
+                  placeholder="Gender"
+                />
+              </div>
             </div>
-          </Container>
+
+            <div className="form-row">
+              <div className="form-group">
+                <label htmlFor="birthdate">
+                  Birthdate <span className="required">*</span>
+                </label>
+                <input
+                  type="date"
+                  id="birthdate"
+                  name="birthdate"
+                  value={formData.birthdate}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="age">
+                  Age <span className="required">*</span>
+                </label>
+                <input
+                  type="number"
+                  id="age"
+                  name="age"
+                  value={formData.age}
+                  onChange={handleChange}
+                  placeholder="Age"
+                />
+              </div>
+            </div>
+
+            <div className="form-row">
+              <div className="form-group">
+                <label htmlFor="email">
+                  Email <span className="required">*</span>
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  placeholder="Email"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="contactNumber">
+                  Contact Number <span className="required">*</span>
+                </label>
+                <input
+                  type="text"
+                  id="contactNumber"
+                  name="contactNumber"
+                  value={formData.contactNumber}
+                  onChange={handleChange}
+                  placeholder="Contact Number"
+                />
+              </div>
+            </div>
+
+            {errorMessage && (
+              <div className="text-danger fw-bold text-center my-3">
+                {errorMessage}
+              </div>
+            )}
+
+            <div className="d-flex justify-content-end mt-4">
+              <button type="submit" className="btn btn-view">
+                Proceed
+              </button>
+            </div>
+          </form>
         </div>
-      </main>
-    </div>
+      </div>
+    </OperationalLayout>
   );
 };
 
-export default CreatePersonnelAccount;
+export default AddDelivery;
