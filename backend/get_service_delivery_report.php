f503808692f6d65a7b76e56935aca2bf931f5490
esang222 <carinagajessa@gmail.com>
2025-10-02 12:58:06 +0800

    BACKEND

--- backend/get_service_delivery_report.php
+++ backend/get_service_delivery_report.php
@@ -1,6 +1,7 @@
 <?php
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Credentials: true");    
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 require 'Database.php';
