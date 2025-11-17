4b81208c45149071cc9481ad13551686af7c68da
cessie840 <maniclangprincessjenin40@gmail.com>
2025-11-18 06:31:06 +0800

    fixed capitalization

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/envirocool_delivery_tracking-web",
+  base: "/Envirocool_Delivery_Tracking-Web",
 });
