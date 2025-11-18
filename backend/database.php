72daf63ceb7bae7ceb3bf3f80166b2479748f078
cessie840 <maniclangprincessjenin40@gmail.com>
2025-11-19 00:09:23 +0800

    testing 123

--- backend/database.php
+++ backend/database.php
@@ -1,7 +1,7 @@
 <?php
 $host = 'localhost';
 $user = 'root';
-$password = '091203';
+$password = 'mabangis';
 $database = 'DeliveryTrackingSystem';
 
 $conn = new mysqli($host, $user, $password, $database);
--- src/Login.jsx
+++ src/Login.jsx
@@ -41,7 +41,7 @@ const Login = () => {
 
     try {
       const response = await axios.post(
-        "https://13.239.143.31/DeliveryTrackingSystem/login.php",
+        "https://ec2-13-239-143-31.ap-southeast-2.compute.amazonaws.com/DeliveryTrackingSystem/login.php",
         { username, password },
         {
           headers: { "Content-Type": "application/json" },
