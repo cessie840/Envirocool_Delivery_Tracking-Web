96002f918b48f23ff0bc80b561d7d9d92e01717f
cessie840 <maniclangprincessjenin40@gmail.com>
2025-11-19 15:52:54 +0800

    renamed base name in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router basename="/Envirocool_Delivery_Tracking-Web">
+    <Router basename="/envirocool-delivery-tracking-web">
       <App />
     </Router>
   </React.StrictMode>
