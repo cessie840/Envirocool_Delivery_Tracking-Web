c57d2371551a36156c4526bbc557a5dfda99c562
cessie840 <maniclangprincessjenin40@gmail.com>
2025-11-19 15:41:19 +0800

    trying vercel again

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Envirocool_Delivery_Tracking-Web",
+  base: process.env.VITE_BASE_PATH || "/envirocool-delivery-tracking-web",
 });
